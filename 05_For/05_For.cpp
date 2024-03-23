f6178df689923159d8cdd5e94ee96c58837dcf1e
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2024-03-23 17:00:42 +0200

    For, Array

---
+++ 05_For/05_For.cpp
@@ -0,0 +1,224 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	/*
+	int i = 0;
+	while (i < 10)
+	{
+		cout << "Hello world" << endl;
+		i++;
+	}
+	cout << endl;
+
+	for (int i = 1; i <= 10; i++)
+	{
+		cout << "The student squatted "<< i  << endl;
+	}
+	for (int i = 0; i < 10; i++)
+	{
+		cout << i << " ";
+	}
+	cout << endl;
+
+	int x;
+	for ( x = 0; x < 10; x+=2)
+	{
+		cout << x << " ";
+	}
+	cout << endl;
+
+	float y = 0;
+	for (   ; y < 1; y+=0.2)
+	{
+		cout << y << " ";
+	}
+	cout << endl;
+
+	for (int i = 1; i <= 5;)
+	{
+		cout << i << " "; i++;
+	}
+	cout << endl;
+
+	for (int i = 0; ; i++)
+	{
+		cout << i << " ";
+		if (i == 15)break;
+	}
+	cout << endl;
+
+	int magicNum = 2;
+	cout << "=================================================" << endl;
+	cout << " \tMy magic number between 1 and 10 "<< endl;
+	cout << "=================================================" << endl;
+	int user = 0;
+	for (int i = 1; i <= 5; i++)
+	{
+		cout << "Enter number : ";
+		cin >> user;
+
+		if (user == magicNum)
+		{
+			cout << "Congratulation !!!" << endl;
+			break;
+		}
+		else
+		{
+			cout << "That's not my number !"<<endl;
+		}
+
+		if (i == 5)
+		{
+			cout << "Try again later ! " << endl;
+		}
+	}
+
+
+	for (int i = 0; i < 26; i++)
+	{
+		/*if (i % 2 != 0)
+			cout << i << " ";*/
+		/*if (i % 2 == 0)
+			continue;
+
+		cout << i << " ";
+	}*/
+
+	//Годинник б, є щогодини стільки разів, скільки зараз
+	//	годин.Написати програму, яка підрахує, скільки разів
+	//	проб, є годинник за 12 годин.
+
+	//int summa = 0;
+	//for (int i = 0; i <= 12; i++)
+	//{
+	//	summa += i;
+	//}
+	//cout << "Hour have puched " << summa << endl;
+
+	//int num, sum = 0;
+	//for (  ; ; )//while (true)
+	//{
+	//	cout << "Enter number : ";
+	//	cin >> num;
+	//	if (num == 0)break;
+
+
+	//	sum += num;
+	//}
+
+	//cout << "Summa all numbers = " << sum << endl;
+
+/*
+	for (int i = 0; i < 7; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			cout << "|###|";
+		}
+		cout << endl;
+	}
+
+	for (int i = 1; i <= 10; i++)
+	{
+		for (int j = 1; j <= 10; j++)
+		{
+			cout << i << " * " << j << " = " << i * j << endl;
+		}
+		cout << endl;
+	}
+
+
+	for (int i = 0; i < 20; i++)
+	{
+		for (int i = 0; i < 20; i++)
+		{
+			cout << "* ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+
+	int row;
+	int star_count;
+	int lenght = 20;
+	row = 1;
+	while (row <= lenght)
+	{
+		star_count = 1;
+		while (star_count <= lenght)
+		{
+			cout << " *"; 
+			star_count++;
+		}
+		cout << endl;
+		row++;
+	}
+*/
+//for (int i = 0; i < 5; i++)
+//{
+//	for (int j = 0; j < 7; j++)
+//	{
+//		cout << "# " ;
+//	}
+//	cout << endl;
+//}
+//cout << endl;
+//for (int i = 0; i < 5; i++)
+//{
+//	for (int j = 0; j < 5; j++)
+//	{
+//		if (i == j)
+//			cout << "+ ";
+//		else
+//			cout << "= ";
+//	}
+//	cout << endl;
+//}
+//cout << endl;
+//
+//for (int i = 0; i < 5; i++)
+//{
+//	for (int j = 0; j < 5; j++)
+//	{
+//		if (i + j == 5 - 1 )
+//			cout << "+ ";
+//		else
+//			cout << "= ";
+//	}
+//	cout << endl;
+//}
+//cout << endl;
+//	
+
+for (int i = 0; i < 10; i++)
+{
+	for (int j = 0; j < 10; j++)
+	{
+		if (i >= j && i+j >= 10-1)
+		{
+			cout << "|===|";
+		}
+		else
+		{
+			cout << "     ";
+		}
+	}
+	cout << endl;
+}
+
+
+
+for (int i = 0; i < 7; i++)
+{
+	for (int j = 0; j < 10; j++)
+	{
+		cout << "|###|";
+	}
+	cout << endl;
+}
+	
+
+}
+
---
+++ 06_Array/06_Array.cpp
@@ -0,0 +1,87 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    //int size = 6;
+
+    int train[3];
+    train[0] = 3;
+    train[1] = 1;
+    train[2] = 4;
+    cout << train[0] << endl;
+    cout << train[1] << endl;
+    cout << train[2] << endl;
+    cout << endl;
+
+
+    int arr[5] = {};
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << " ";
+    } 
+    cout << endl;
+
+    const int size = 10;
+    int arr2[size] = {1,2,3,4,5,6,7,8,9,10};
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr2[i] << " ";
+    }
+    cout << endl;
+
+    int arr3[5] = { 1,2,3 };
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr3[i] << " ";
+    }
+    cout << endl;
+
+    int arr4[] = { 10,20,30,40,50,55,58,47,69 };
+    for (int i = 0; i < 9; i++)
+    {
+        cout << arr4[i] << " ";
+    }
+    cout << endl;
+    const int Size = 3;
+    int arr5[Size];
+    for (int i = 0; i < Size; i++)
+    {
+        cout << "Enter [ "<< i + 1 <<" ]" << "number : "; 
+        cin >> arr[i];
+    }
+    for (int i = 0; i < Size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    //Написати програму, яка знаходить суму всіх від'ємних значень у масиві.
+
+    //Написати програму, яка знаходить мінімальне й максимальне значення в масиві 
+    // і виводить їх на екран.
+
+    const int SIZE = 10;
+    int arr6[SIZE] = { 23,-1,8,-7,15,-4,9,-6,33,7 };
+    int sum = 0;
+    int max = arr6[0];
+    int min =arr6[0];
+    for (int i = 0; i < SIZE; i++)
+    {
+        if (arr6[i] < 0)
+            sum += arr6[i];
+
+        if (arr6[i] > max)
+            max = arr6[i];
+
+        if (arr6[i] < min)
+            min = arr6[i];
+    }
+    cout << "Summa elements of arr = " << sum << endl;
+    cout << "Max element of arr = " << max << endl;
+    cout << "Min element of arr = " << min << endl;
+
+
+
+
+}
