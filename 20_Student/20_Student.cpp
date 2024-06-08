c3efa1905b3044852ed77f1d09755f0804ff3621
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2024-06-08 14:59:37 +0300

    Copy constructor

---
+++ 20_Student/20_Student.cpp
@@ -0,0 +1,287 @@
+#include <iostream>
+using namespace std;
+
+struct Date
+{
+//public:
+    int day;
+    int month;
+    int year;
+};
+
+class Student//incapsulation
+{
+private:
+    string name;//OLena
+    string last_name;
+    string surname;
+    Date birthday;
+    string phone ;
+    string city;
+    string country;
+    string education_house;
+    string education_city;
+    string education_country;
+    string group_number;
+    int* marks;
+    int markCount;
+    int average;
+    void AverageMark()
+    {
+        float sum = 0;
+        for (int i = 0; i < markCount; i++)
+        {
+            sum += marks[i];
+        }
+        average = (float)sum / markCount;
+    }
+public:
+    ////ctor + TAB
+    Student()//default constructor
+    {
+        cout << "Constructor" << endl;
+        name = "no name";
+        last_name = "no last name";
+        surname = "no surname";
+        birthday = { 0,0,0 };
+        phone = "no phone";
+        city = "no city";
+        country = "no country";
+        education_house = "no education house";
+        education_city = "no education city";
+        education_country = "no education country";
+        group_number = " no group";
+        marks = nullptr;
+        markCount = 0;
+        average = 0;
+    }
+    //parametrized constructor
+    Student(string name, string lastname, string surname)
+    {
+        cout << "Ctor invocation......." << endl;
+        this->name = name;
+        this->last_name = lastname;
+        this->surname = surname;
+        marks = nullptr;
+        markCount = 0;
+        average = 0;
+    }
+    Student(string name, string lastname, string surname, Date birthday)
+    {
+        cout << "Ctor invocation......." << endl;
+        this->name = name;
+        this->last_name = lastname;
+        this->surname = surname;
+        this->birthday = birthday;
+        marks = nullptr;
+        markCount = 0;
+        average = 0;
+    }
+    Student(string name, string lastname, string surname, Date birthday, string phone)
+    {
+        cout << "Ctor invocation......." << endl;
+        this->name = name;
+        this->last_name = lastname;
+        this->surname = surname;
+        this->birthday = birthday;
+        this->phone = phone;
+        marks = nullptr;
+        markCount = 0;
+        average = 0;
+    }
+    void InitDefault()
+    {
+        name = "no name";
+        last_name = "no last name";
+        surname = "no surname";
+        birthday = { 0,0,0 };
+        phone = "no phone";
+        city = "no city";
+        country = "no country";
+        education_house = "no education house";
+        education_city = "no education city";
+        education_country = "no education country";
+        group_number = " no group";
+    }
+    void InputFromKeyboard()
+    {
+        cout << "Enter name : "; cin >> name;
+        cout << "Enter last name : "; cin >> last_name;
+        cout << "Enter surname : "; cin >> surname;
+        cout << "Enter birthday day : "; cin >> birthday.day;
+        cout << "Enter birthday month : "; cin >> birthday.month;
+        cout << "Enter birthday year : "; cin >> birthday.year;
+        cout << "Enter phone : "; cin >> phone;
+        cout << "Enter city : "; cin >> city;
+        cout << "Enter country : "; cin >> country;
+        cout << "Enter education institution : "; cin >> education_house;
+        cout << "Enter education city : "; cin >> education_city;
+        cout << "Enter education country : "; cin >> education_country;
+        cout << "Enter group_number : "; cin >> group_number;
+    }
+    void Show()
+    {
+        cout << "Name : " << name << endl;
+        cout << "Last name : " << last_name << endl;
+        cout << "Surname : " << surname << endl;
+        cout << "Birthday day  : " << birthday.day << endl;
+        cout << "Birthday month  : " << birthday.month << endl;
+        cout << "Birthday year  : " << birthday.year << endl;
+        cout << "Phone : " << phone << endl;
+        cout << "City : " << city << endl;
+        cout << "Country : " << country << endl;
+        cout << "education institution : " << education_house << endl;
+        cout << "education city : " << education_city << endl;
+        cout << "education country : " << education_country << endl;
+        cout << "Group : " << group_number << endl;
+        cout << "Rating : " << average << endl;
+        cout << "Marks : ";
+        for (int i = 0; i < markCount; i++)
+        {
+            cout << marks[i] << " ";
+        }
+        cout << endl;
+    }
+    void AddMark(int mark)
+    {
+        markCount++;
+        int* temp = new int[markCount];
+        for (int i = 0; i < markCount-1; i++)
+        {
+            temp[i] = marks[i];
+        }
+        temp[markCount - 1] = mark;
+        if (marks != nullptr)
+            delete[]marks;
+        marks = temp;
+        AverageMark();
+    }
+    //access : set get
+    void setName(string name)
+    {
+        this->name = name;
+        //name = n;
+    }
+    string getName()
+    {
+        return name;
+    }
+    void setLastName(string last_name)
+    {
+        this->last_name = last_name;
+    }
+    string getLastName()
+    {
+        return last_name;
+    }
+    void setSurname(string surname)
+    {
+        this->surname = surname;
+    }
+    string getSurname()
+    {
+        return surname;
+    }
+    void setBirthday(Date birthday)
+    {
+        this->birthday = birthday;
+    }
+    Date getBirthday()
+    {
+        return birthday;
+    }
+    void setPhone(string phone)
+    {
+        this->phone = phone;
+    }
+    string getPhone()
+    {
+        return phone;
+    }
+    void setCity(string city)
+    {
+        this->city = city;
+    }
+    string getCity()
+    {
+        return city;
+    }
+    void setCountry(string country)
+    {
+        this->country = country;
+    }
+    string getCountry()
+    {
+        return country;
+    }
+    void setGroupNumber(string group)
+    {
+        this->group_number = group;
+    }
+    string getGroupNUmber()
+    {
+        return group_number;
+    }
+    //~  + Tab
+    ~Student()//destructor
+    {
+        cout << "Destructor  "<<name  << endl;
+        if (marks != nullptr)
+            delete[]marks;
+    }
+};
+
+class Car//destructor constructor
+{
+
+};
+int main()
+{    
+    int a = 5;
+    int* pb = new int(5);
+   
+    
+    
+    Car car;//default constructor
+
+    Student student;//default constructor
+    //student.InitDefault();
+    student.setName("Tania");
+    student.Show();
+
+    Student st1;//default constructor
+    //st1.InitDefault();
+    st1.setName("Vasia");
+    st1.Show();
+
+
+    Student newStudent("Olena", "Sergievna", "Iliushyn");
+    newStudent.Show();
+
+    Student st2("Mukola", "Petrovuch", "Petruk", { 11,5,2004 });
+    st2.Show();
+
+    Student st3("Olga", "Ivanivna", "Semenuc", { 5,5,2005 }, "+380956321478");
+    st3.Show();
+    st3.AddMark(12);
+    st3.AddMark(7);
+    st3.AddMark(4);
+    st3.AddMark(8);
+    st3.AddMark(11);
+    st3.AddMark(11);
+    st3.AddMark(11);
+    st3.Show();
+    //student.setLastName("Ivanivna");
+    //student.setSurname("Oliunuk");
+    //student.setBirthday({ 11,12,2000 });
+    //student.setCity("Rivne");
+    //student.setCountry("Ukaine");
+    //student.setGroupNumber("SPD311");
+    //student.setPhone("+380974562585");
+    //cout << "Name : " << student.getName() << endl;
+    ////student.InputFromKeyboard();
+    //student.Show();
+    //
+    delete pb;
+}
+
---
+++ 21_CopyConstructor/21_CopyConstructor.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+using namespace std;
+
+struct Point//public
+{
+    int x;
+    int y;
+};
+class Student//private
+{
+private:
+    string name;
+    int age;
+    int* marks;
+    int countMark;
+public:
+    Student()//default constructor
+    {
+        name = "no name";
+        age = 0;
+        marks = nullptr;
+        countMark = 0;
+    }
+    Student(string name, int age)//parametrized constructor
+    {
+        this->name = name;//Bob = Bob
+        this->age = age;//   12 = 12
+        marks = nullptr;//0x0000017f97742dc0 = 0x0000017f97742dc0
+
+        countMark = 0;//4 = 4
+    }
+    Student(const Student &other)
+    {
+        this->name = other.name;
+        this->age = other.age;
+        //this->marks = other.marks;//copy pointer
+        this->marks = new int[other.countMark];
+        for (int i = 0; i < other.countMark; i++)
+        {
+            this->marks[i] = other.marks[i];
+        }
+        this->countMark = other.countMark;
+
+    }
+    void AddMark(int mark)
+    {
+        countMark++;
+        int* temp = new int[countMark];
+        for (int i = 0; i < countMark - 1; i++)
+        {
+            temp[i] = marks[i];
+        }
+        temp[countMark - 1] = mark;
+        if (marks != nullptr)
+            delete[]marks;
+        marks = temp;
+    }
+    void Print()
+    {
+        cout << "Name : " << name << " . Age : " << age << endl;
+        cout << "Marks : ";
+        for (int i = 0; i < countMark; i++)
+        {
+            cout << marks[i] << " ";
+        }cout << endl;
+    }
+    ~Student()
+    {
+        if (marks != nullptr)
+            delete[]marks;
+        cout << "Destructor " << name << endl;
+    }
+};
+
+void Add(int &a)
+{
+    a++;
+    cout << a << endl;
+}
+int main()
+{
+    int a = 5;
+    cout << a << endl;
+    Add(a);
+    cout << a << endl;
+
+    Point p;
+    Student st;//default constructor
+    st.Print();
+
+    Student student("Bob", 12);//parametrized constructor
+    student.Print();
+    student.AddMark(12);
+    student.AddMark(7);
+    student.AddMark(8);
+    student.AddMark(9);
+    student.Print();
+    Student copy(student);
+    copy.Print();
+
+
+    /*
+    int a = 0;
+    cout << a << endl;
+    a = 5;
+    cout << a << endl;
+    int b = 10;
+    cout << b << endl;
+    int c = 1.33;
+    cout << c << endl;
+
+    int numbers[2];
+    cout << numbers[0] << endl;
+    numbers[0] += 2;
+    cout << numbers[0] << endl;
+
+    int number = 0.33;//ініціалізація з копіюванням
+    cout << number << endl;
+    int value(2.5);//пряма ініціалізація
+    cout << value << endl;
+    int size { 33};//уніфікована ініціалізація
+    //cout << size << endl;
+
+    int answer{ 12 };
+    const float goodTemp{ 36.6 };
+    int grades[4]{ 11,12,10,9 };
+    int matrix[2][2]{ {5,6},{7,8} };
+    int* ptr = &answer;
+    int* ptrData{ nullptr };
+    Point point{ 5,10 };
+    */
+    
+
+
+
+
+
+
+
+}
